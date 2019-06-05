38f65bfec5928fa62eb5f7d2cc6ae3d6f757d5df
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-05 12:58:20 -0400

    test compound statement to split lines

---
+++ test.py
@@ -0,0 +1,16 @@
+# Open test.txt and set to variable
+#ainulindalë = open("test.txt", "r")
+
+# Prints the text of test.txt as one line
+#print(ainulindalë.read())
+
+# Open test.txt with read privileges and set as variable "ainulindalë"
+with open("test.txt", "r") as ainulindalë:
+    # For each line in ainulindalë, do the next thing
+    for line in ainulindalë:
+        # Split each word into its own line
+        for word in line.split():
+            print(word)
+
+# Import regex for stripping punctuation/spaces
+import re
